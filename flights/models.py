9774fa2dba9c94caf46b35810d8361539ba91c17
Dragos Catarahia <dragos@catarahia.me>
2024-05-01 09:25:39 +0800

    Add BaseModel

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    # 'updated_at' is nullable and only set when an object is updated for the first time.
+    # This helps distinguish between objects that have never been modified and those that have.
+    # Potential issue: Setting 'updated_at' on new objects with a value different from 'created_at'
+    # may give the impression that the object has been modified, even if it has not.
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["created_at"]
