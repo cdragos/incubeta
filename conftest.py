4a66f07bc2ecab2a8932d5be1d92aef3c530b726
Dragos Catarahia <dragos@catarahia.me>
2024-05-01 11:40:05 +0800

    Use sqlite memory for the unit tests

--- conftest.py
+++ conftest.py
@@ -1,1 +1,11 @@
+import pytest
 
+
+@pytest.fixture(autouse=True)
+def use_sqlite_memory_database(settings):
+    settings.DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    }
